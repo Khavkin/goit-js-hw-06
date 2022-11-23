1dfc7f746a34c9f0683275d369abf86dacad75bf
Khavkin <hdm@ukr.net>
2022-11-23 23:31:45 +0200

    added resolved task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const loginForm = document.querySelector('.login-form');
 
+const submitForm = event => {
+  event.preventDefault();
+  const inputs = [...event.currentTarget.elements].filter(({ tagName }) => tagName === 'INPUT');
+
+  if (inputs.every(({ value }) => value !== '')) {
+    let result = {};
+    inputs.forEach(({ name, value }) => (result[name] = value));
+    console.log(result);
+    loginForm.reset();
+  } else alert('Все поля формы должны быть заполнены!');
+};
+
+loginForm.addEventListener('submit', submitForm);
