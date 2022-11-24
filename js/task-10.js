a1a5d927f09e1c27782a638b49333b3b4395287d
Khavkin <hdm@ukr.net>
2022-11-24 21:45:39 +0200

    add resolved task 10 and fixed task 09

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,43 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+const amount = document.querySelector('#controls > input');
+
+const targetDiv = document.querySelector('#boxes');
+
+const createBoxes = amount => {
+  let markup = '';
+  const minSize = 30;
+  destroyBoxes();
+  for (let i = 0; i < amount; i += 1) {
+    markup =
+      markup +
+      `<div style="background-color:${getRandomHexColor()}; width:${minSize + i * 10}px;height:${
+        minSize + i * 10
+      }px"></div>`;
+  }
+  targetDiv.insertAdjacentHTML('afterbegin', markup);
+};
+
+const destroyBoxes = () => {
+  targetDiv.innerHTML = '';
+};
+
+const btnCreateOnClick = event => {
+  event.preventDefault();
+  if (amount.value > 0 && amount.value <= 100) createBoxes(amount.value);
+  else alert('Количество должно быть в промежутке от 1 до 100');
+};
+
+const btnDestroyOnClick = event => {
+  event.preventDefault();
+  destroyBoxes();
+};
+
+btnCreate.addEventListener('click', btnCreateOnClick);
+btnDestroy.addEventListener('click', btnDestroyOnClick);
