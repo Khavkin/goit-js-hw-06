fb1420b1ec39601ec9c0634581a0aa5cd7bc390b
Khavkin <hdm@ukr.net>
2022-11-24 20:05:17 +0200

    add resolved task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColor = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+const body = document.querySelector('body');
+
+btnChangeColor.addEventListener('click', event => {
+  const color = getRandomHexColor();
+  spanColor.textContent = color;
+  body.style.backgroundColor = color;
+});
